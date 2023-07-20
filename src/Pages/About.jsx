32cfbf9171dfa4ab4b9e0ad4cf37ef02ce74a380
Maximiliano Quintana <urumax@gmail.com>
2023-07-20 00:28:03 -0300

    about v2

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,7 @@ import "./About.css";
 
 function About() {
   return (
-    <div className="container m-auto d-flex flex-column align-items-center pt-5">
+    <div className="container m-auto d-flex flex-column align-items-center pt-5 py-5">
       <div className="row d-flex mt-5">
         <div className="col-12 col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
           <img
@@ -15,22 +15,39 @@ function About() {
           <h1 className="about-title">About me</h1>
 
           <p className="mt-4 about-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam alias
-            corporis voluptas ducimus dignissimos consectetur culpa, dolore sint
-            enim modi doloremque aliquam similique rem nulla aperiam minima est
-            fuga placeat.
+            Hello! My name is Maximiliano Quintana, and I'm from Uruguay.
           </p>
+          <p className="mt-1 about-text">
+            With a solid 12-year background as a System Administrator in IT
+            Support and Infrastructure areas, I made a career change in 2023 and
+            graduated as a Full Stack Developer.
+          </p>
+          <p className="mt-1 about-text">
+            Currently, I'm eagerly seeking my first experience in the
+            development field, where I can apply my acquired experience and
+            unleash my potential. I am passionate about working on challenging
+            projects, and I am excited to contribute my skills in this
+            ever-evolving domain.
+          </p>
+          <p className="mt-1 about-text">Thank you for visiting my site. </p>
         </div>
       </div>
+
       <div className="row d-flex mt-5 w-auto">
+        <hr className="hr-format" />
         <div className="col-12 col-sm-12 col-md-6 col-lg-8">
           <h1 className="about-title">About the project</h1>
 
           <p className="mt-4 about-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam alias
-            corporis voluptas ducimus dignissimos consectetur culpa, dolore sint
-            enim modi doloremque aliquam similique rem nulla aperiam minima est
-            fuga placeat.
+            This proyect was made during Hack Academy's Coding Bootcamp, a
+            full-time course with +600 hours of dedication over 3 months while
+            going through the React.js part of the Bootcamp.
+          </p>
+          <p className="mt-1 about-text">
+            Its purpose is to be a web application for streaming movies that
+            provides detailed information about them. It was designed as a
+            single page application, using React to layout the site and querying
+            with AJAX calls to The Movie Database (TMDB)'s API.
           </p>
         </div>
         <div className="col-12 col-sm-12 col-md-6 col-lg-4 text-center ">
@@ -54,8 +71,46 @@ function About() {
             </div>
           </div>
 
-          <div className="about-technologies-container">
+          <div className="about-technologies-container mt-5">
             <h4 className="about-card-text">Technologies</h4>
+            <div className="d-flex justify-content-center gap-4 mt-3">
+              <div>
+                <img
+                  src="img/html5-logo-svgrepo-com.svg"
+                  alt="html5"
+                  className="techs-icons"
+                  style={{ height: "50px", width: "auto", color: "white" }}
+                />
+                <h5 className="techs-texts">HTML5</h5>
+              </div>
+              <div>
+                <img
+                  src="img/css-4.svg"
+                  alt="html5"
+                  className="techs-icons"
+                  style={{ height: "50px", width: "auto", color: "white" }}
+                />
+                <h5 className="techs-texts">CSS</h5>
+              </div>
+              <div>
+                <img
+                  src="img/logo-javascript-svgrepo-com.svg"
+                  alt="html5"
+                  className="techs-icons"
+                  style={{ height: "50px", width: "auto", color: "white" }}
+                />
+                <h5 className="techs-texts">JavaScript</h5>
+              </div>
+              <div>
+                <img
+                  src="img/logo-react-svgrepo-com.svg"
+                  alt="html5"
+                  className="techs-icons"
+                  style={{ height: "50px", width: "auto", color: "white" }}
+                />
+                <h5 className="techs-texts">React</h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
